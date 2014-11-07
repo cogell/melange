115182832df399c1b5997fa389ebba5ba3f72f1e
B. Cedric Cogell <cedric.cogell@gmail.com>
2014-11-07 15:13:27 -0500

    adds sourcemaps and minification #19654906863543

--- gulp/tasks/plumbing/app.js
+++ gulp/tasks/plumbing/app.js
@@ -1,12 +1,19 @@
-var gulp     = require('gulp');
-var taskname = require('path').basename(__filename, '.js');
-var config   = require('./../../config');
+var gulp       = require('gulp');
+var taskname   = require('path').basename(__filename, '.js');
+var config     = require('./../../config');
 
-var concat   = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+var concat     = require('gulp-concat');
+var uglify     = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate'); // keep uglify from mangling impliciently annotated injected strings, see "Implicit Annotation": https://docs.angularjs.org/guide/di
 
 gulp.task( taskname, function () {
   // make sure to concat all the module definition files before anything else
   gulp.src(['./src/**/*.mod.js', './src/**/*.js', '!src/**/*.test.js'])
-    .pipe( concat( config.outputAppJsFile ) )
+    .pipe( sourcemaps.init() )
+      .pipe( concat( config.outputAppJsFile ) )
+      .pipe( ngAnnotate() )
+      .pipe( uglify() )
+    .pipe( sourcemaps.write() )
     .pipe( gulp.dest( config.outputDirectory ) );
 });
