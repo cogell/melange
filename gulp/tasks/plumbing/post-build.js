5df2656ee11fc5742fbc045e398fb5b489da6c8d
B. Cedric Cogell <cedric.cogell@gmail.com>
2014-11-15 09:39:55 -0500

    adds production flag for post-build task #20281175737318

--- gulp/tasks/plumbing/post-build.js
+++ gulp/tasks/plumbing/post-build.js
@@ -1,11 +1,13 @@
 var gulp        = require('gulp');
 var taskname    = require('path').basename(__filename, '.js');
+var argv        = require('yargs').argv;
 
 var runSequence =  require('run-sequence');
 
 gulp.task( taskname, function (done) {
-  // if (production)
-  // runSequence('rev', 'rev-replace', 'clean-build', done);
-  // else do nothing
-  done();
+  if (argv.production) {
+    runSequence('rev', 'rev-replace', 'clean-build', done);
+  } else {
+    done();
+  }
 });
