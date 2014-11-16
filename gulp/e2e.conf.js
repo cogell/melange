8b17eb52ce08932d819fee3f170eed3651b4ff5e
B. Cedric Cogell <cedric.cogell@gmail.com>
2014-11-15 21:24:18 -0500

    more tweaks....

--- gulp/e2e.conf.js
+++ gulp/e2e.conf.js
@@ -36,8 +36,11 @@ if (process.env.TRAVIS) {
   config.sauceUser = process.env.SAUCE_USERNAME;
   config.sauceKey = process.env.SAUCE_ACCESS_KEY;
   config.capabilities = _.extend(config.capabilities, {
+    'username': process.env.SAUCE_USERNAME,
+    'accessKey': process.env.SAUCE_ACCESS_KEY,
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'Melange E2E Tests'
   });
 }
 
