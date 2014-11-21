64337f5503c88944e82bf50abaa9b71b77ae7788
B. Cedric Cogell <cedric.cogell@gmail.com>
2014-11-21 14:45:25 -0500

    further removing travs-saucelabs connect artifacts

--- gulp/e2e.conf.js
+++ gulp/e2e.conf.js
@@ -1,5 +1,4 @@
 // config for protractor
-var _ = require('lodash');
 
 var config = {
   allScriptsTimeout: 11000,
@@ -26,20 +25,5 @@ var config = {
   }
 
 };
-
-// If this is running on [Travis](http://travis-ci.org) then we add additional details to run the tests on the
-// [Sauce labs](https://saucelabs.com/opensource/travis) cloud.
-if (process.env.TRAVIS) {
-  config.seleniumAddress = 'http://localhost:4445/wd/hub';
-  // config.sauceUser = process.env.SAUCE_USERNAME;
-  // config.sauceKey = process.env.SAUCE_ACCESS_KEY;
-  config.capabilities = _.extend(config.capabilities, {
-    'username': process.env.SAUCE_USERNAME,
-    'accessKey': process.env.SAUCE_ACCESS_KEY,
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': 'Melange E2E Tests'
-  });
-}
 
 exports.config = config;
