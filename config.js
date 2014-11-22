26b2c11ae4862451b2267e8479cb6c8f9214d565
B. Cedric Cogell <cedric.cogell@gmail.com>
2014-11-22 17:32:58 -0500

    adds angular-ui-router in place of angular-route

--- config.js
+++ config.js
@@ -10,7 +10,11 @@ System.config({
     "angular": "github:angular/bower-angular@^1.3.3",
     "angular-mocks": "github:angular/bower-angular-mocks@^1.3.3",
     "angular-route": "github:angular/bower-angular-route@^1.3.3",
+    "angular-ui-router": "github:angular-ui/ui-router@^0.2.13",
     "text": "github:systemjs/plugin-text@^0.0.2",
+    "github:angular-ui/ui-router@0.2.13": {
+      "angular": "github:angular/bower-angular@^1.3.2"
+    },
     "github:angular/bower-angular-route@1.3.3": {
       "angular": "github:angular/bower-angular@^1.3.0"
     }
@@ -19,6 +23,7 @@ System.config({
 
 System.config({
   "versions": {
+    "github:angular-ui/ui-router": "0.2.13",
     "github:angular/bower-angular": "1.3.3",
     "github:angular/bower-angular-mocks": "1.3.3",
     "github:angular/bower-angular-route": "1.3.3",
--- src/apps/home/home.conf.js
+++ src/apps/home/home.conf.js
@@ -1,8 +1,9 @@
 import homeTmpl from 'apps/home/home.tmpl.html!text'
 
-export default function ($routeProvider) {
-  $routeProvider
-    .when('/', {
+export default function ($stateProvider) {
+  $stateProvider
+    .state('home', {
+      url: '',
       template: homeTmpl
     });
 }
